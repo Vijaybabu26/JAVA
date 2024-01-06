b143f44c7342573c5212c379ff5dada782ad0f04
Vijaybabu26 <vijaybabuponnada1@gmail.com>
2024-01-06 19:11:42 +0530

    Start

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class demo{
+	public static void main(String [] args){
+		int a = 20,b = 30, sum = a + b ;
+		System.out.println("The Value of the a is "+a);
+		System.out.println("The Value of the b is "+b);
+		System.out.println("The Value of the a + b is "+sum);
+	}
+}
