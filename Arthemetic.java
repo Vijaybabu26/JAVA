ae2ebb07a5086bbd7751fcb2a7ccd3ea5db54d97
Vijaybabu26 <vijaybabuponnada1@gmail.com>
2024-01-12 21:17:04 +0530

    Arthemetic program

---
+++ Arthemetic.java
@@ -0,0 +1,21 @@
+
+public class Arthemetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 100; 
+		int b = 50;
+		System.out.print("The Value of a+b is ");
+    System.out.println(a+b);
+    System.out.print("The Value of a-b is ");
+    System.out.println(a-b);
+    System.out.print("The Value of a/b is ");
+    System.out.println(a/b);
+    System.out.print("The Value of a*b is ");
+    System.out.println(a*b);
+    System.out.print("The Value of a%b is ");
+    System.out.println(a%b);
+	}
+
+}
