42272f05dfa4646428c62358fd601c4cf0082fdc
Vijaybabu26 <vijaybabuponnada1@gmail.com>
2024-01-30 18:32:39 +0530

    Java Programs File Has Been Created

---
+++ Java Programs/Shape.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Shape {
+
+	public static void main(String[] args) {
+		System.out.print("Main");
+	}
+	public void shape() {
+		System.out.println("Shape");
+	}
+	public void traingle() {
+		System.out.println("Traingle");
+	}
+	public void rectangle() {
+		System.out.println("rectangle");
+	}
+
+}
+
+ 
