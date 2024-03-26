f70c83e55923a8a121ba7a86692c7368ba678bab
Vijaybabu26 <155373404+Vijaybabu26@users.noreply.github.com>
2024-03-26 10:27:00 +0530

    Add files via upload

---
+++ Java Programs/Paymentcli.sql
@@ -0,0 +1,55 @@
+create database Paymentapp;
+Show Databases;
+use Paymentapp;
+CREATE TABLE User_Info (
+    User_Id INT NOT NULL,
+    First_Name VARCHAR(25),
+    Last_Name VARCHAR(25),
+    Phone_No varchar(15),
+    Date_Of_Birth VARCHAR(25),
+    Address VARCHAR(100),
+    PassWord VARCHAR(25),
+    Curr_Wallet_Balance double,
+    PRIMARY KEY (User_Id)
+);
+ALTER TABLE User_Info
+MODIFY User_Id INT NOT NULL AUTO_INCREMENT;
+show tables;
+-- drop table User_Info;
+describe User_Info;
+CREATE TABLE Bank_Account_Details (
+    Bank_AcctNo INT NOT NULL,
+    Bank_AcctBankName VARCHAR(25),
+    Acct_TypeId int,
+    Bank_IFSC_Code VARCHAR(10),
+    Bank_AcctPin INT,
+    User_Id INT,
+    Curr_Bank_Balance double,
+    FOREIGN KEY (User_Id) REFERENCES User_Info (User_Id),
+    FOREIGN KEY (Acct_TypeId) REFERENCES Acct_Types(Bank_AcctTypeId),
+    PRIMARY KEY (Bank_AcctNo)
+);
+ describe BankAccount;
+ -- drop table BankAccount;
+ 
+ Create Table Acct_Types(
+	Bank_AcctTypeId int NOT NULL,
+    Bank_AcctTypeCode varchar(3),
+    Bank_AcctTypeDesc varchar(20),
+    primary Key (Bank_AcctTypeId)
+ );
+ 
+ 
+ drop table Acct_Types;
+ CREATE TABLE Transaction (
+    Txn_Id Int NOT NULL,
+    Txn_Date date,
+    Txn_Amount DOUBLE,
+    Txn_Type Enum ("CREDIT","DEBIT"),
+    Txn_AcctType enum("BANK_ACCOUNT","WALLET","CASH"),
+    Txn_User_Id int,
+    FOREIGN KEY (Txn_User_Id) REFERENCES User_Info (User_Id),
+    PRIMARY KEY (Txn_Id)
+);
+  describe Transaction;
+ drop table Transaction;
