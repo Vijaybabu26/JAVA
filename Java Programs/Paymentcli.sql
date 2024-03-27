9e3f4193e3b16201d76106d535dcd7d7ac30f744
Vijaybabu26 <155373404+Vijaybabu26@users.noreply.github.com>
2024-03-27 15:49:00 +0530

    Add files via upload

--- Java Programs/Paymentcli.sql
+++ Java Programs/Paymentcli.sql
@@ -1,8 +1,7 @@
 create database Paymentapp;
-Show Databases;
 use Paymentapp;
 CREATE TABLE User_Info (
-    User_Id INT NOT NULL,
+    User_Id INT auto_increment not NULL,
     First_Name VARCHAR(25),
     Last_Name VARCHAR(25),
     Phone_No varchar(15),
@@ -12,44 +11,32 @@ CREATE TABLE User_Info (
     Curr_Wallet_Balance double,
     PRIMARY KEY (User_Id)
 );
-ALTER TABLE User_Info
-MODIFY User_Id INT NOT NULL AUTO_INCREMENT;
-show tables;
--- drop table User_Info;
-describe User_Info;
+Select * from User_Info;
+Update User_Info set Curr_Wallet_Balance = Curr_Wallet_Balance + 800 where User_Id = 2;
+Select * from Bank_Account_Details;
+Update Bank_Account_Details Set Curr_Bank_Balance = Curr_Bank_Balance + 300 where Bank_AcctNo = 1234;
 CREATE TABLE Bank_Account_Details (
     Bank_AcctNo INT NOT NULL,
     Bank_AcctBankName VARCHAR(25),
-    Acct_TypeId int,
-    Bank_IFSC_Code VARCHAR(10),
+    Acct_TypeId Varchar(15) Not NUll,
+    Bank_IFSC_Code VARCHAR(20),
     Bank_AcctPin INT,
-    User_Id INT,
+    Bank_User_Id INT,
     Curr_Bank_Balance double,
-    FOREIGN KEY (User_Id) REFERENCES User_Info (User_Id),
-    FOREIGN KEY (Acct_TypeId) REFERENCES Acct_Types(Bank_AcctTypeId),
+    FOREIGN KEY (Bank_User_Id) REFERENCES User_Info (User_Id),
     PRIMARY KEY (Bank_AcctNo)
 );
- describe BankAccount;
- -- drop table BankAccount;
- 
- Create Table Acct_Types(
-	Bank_AcctTypeId int NOT NULL,
-    Bank_AcctTypeCode varchar(3),
-    Bank_AcctTypeDesc varchar(20),
-    primary Key (Bank_AcctTypeId)
- );
- 
- 
- drop table Acct_Types;
- CREATE TABLE Transaction (
-    Txn_Id Int NOT NULL,
+drop table Bank_Account_Details;
+CREATE TABLE Transaction (
+    Txn_Id varchar(45),
     Txn_Date date,
     Txn_Amount DOUBLE,
-    Txn_Type Enum ("CREDIT","DEBIT"),
-    Txn_AcctType enum("BANK_ACCOUNT","WALLET","CASH"),
+    Txn_Type varchar(25),
+    Txn_AcctType_Source Varchar(25),
+    Txn_AcctType_Dest Varchar(25),
     Txn_User_Id int,
     FOREIGN KEY (Txn_User_Id) REFERENCES User_Info (User_Id),
     PRIMARY KEY (Txn_Id)
 );
-  describe Transaction;
- drop table Transaction;
+select * from Transaction;
+drop table Transaction;
