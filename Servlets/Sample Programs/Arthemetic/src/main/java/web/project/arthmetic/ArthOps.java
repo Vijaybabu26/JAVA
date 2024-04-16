bd0e9d2078244963aacdc5349c5a46cc26d21512
Vijaybabu26 <155373404+Vijaybabu26@users.noreply.github.com>
2024-04-16 15:46:18 +0530

    Add files via upload

---
+++ Servlets/Sample Programs/Arthemetic/src/main/java/web/project/arthmetic/ArthOps.java
@@ -0,0 +1,46 @@
+package web.project.arthmetic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class ArthOps extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public ArthOps() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//			System.out.println("Welcome");
+//			System.out.println(request.getParameter("num1"));
+//			System.out.println(request.getParameter("num2"));
+//			System.out.println(request.getParameter("num3"));
+			String num1 = request.getParameter("num1");
+			String num2 = request.getParameter("num2");
+			String num3 = request.getParameter("num3");
+			int res = Integer.parseInt(num1)+Integer.parseInt(num3);
+			
+			
+		
+			
+			response.getWriter().write("<html>\r\n"
+					+ "    <body>\r\n"
+					+ "        <h1>THE CALCULATION OF </h1>\r\n"
+					+ "        <h2> "+ res+ "</h2>\r\n"
+					+ "    </body>\r\n"
+					+ "</html>");
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}
